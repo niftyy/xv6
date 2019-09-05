fb106b8e285cbc76e93d610da00b9a0cf89db328
niftyy <tusharsparte@gmail.com>
2019-09-05 11:29:21 +0530

    added process counter to ps

--- proc.c
+++ proc.c
@@ -544,15 +544,22 @@ cps()
   // loop over process table looking for process with pid.
   acquire(&ptable.lock);
   cprintf("name \t pid \t state \t \n");
+  int count = 0;
   for(p=ptable.proc; p<&ptable.proc[NPROC]; p++){
-    if(p->state == SLEEPING)
+    if(p->state == SLEEPING){
+      count++;
       cprintf("%s \t %d \t SLEEPING \t \n", p->name, p->pid);
-    else if(p->state == RUNNING)
+    }
+    else if(p->state == RUNNING){
+      count++;
       cprintf("%s \t %d \t RUNNING \t \n", p->name, p->pid);
-    else if(p->state == RUNNABLE)
+    }
+    else if(p->state == RUNNABLE){
+      count++;
       cprintf("%s \t %d \t RUNNABLE \t \n", p->name, p->pid);
+    }
   }
-
+  cprintf("Number of processes: %d\n", count);
   release(&ptable.lock);
 
   return 22;
